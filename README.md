2dd27e561d909b410e26314a8ce7e7e7d4827df8
Johnny <bizzel@virginmedia.com>
2017-01-08 03:44:42 +0000

    First commit - almost all done

---
+++ README.md
@@ -0,0 +1,21 @@
+### Image Search Abstraction Layer
+
+For this challenge I paired with Parminder Sanghera (ajitsy)
+We decided to use the flickr API as I have previously used this for the Weather App
+
+The format is slightly different from other APIs.
+
+The API is called by using
+`https://img-abstraction-layer-jb-ps-bizzel.c9users.io/api/`
+
+then adding a **search term**
+
+eg) `https://img-abstraction-layer-jb-ps-bizzel.c9users.io/api/cats`
+
+To show another **page** of results use query strings
+
+eg) `https://img-abstraction-layer-jb-ps-bizzel.c9users.io/api/cats?page=2`
+
+To limit the results use **pagelimit** in the query string
+
+eg) `https://img-abstraction-layer-jb-ps-bizzel.c9users.io/api/cats?page=2&pagelimit=5`
---
+++ routes/api.js
@@ -0,0 +1,157 @@
+var express = require('express');
+var router = express.Router();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var fs = require('fs');
+var moment = require("moment");
+var dotenv = require('dotenv');
+dotenv.load();
+
+
+function callAjax(url, callback){
+    var xmlhttp;
+    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function readHistory(res) {
+    var content;
+    // First I want to read the file
+    fs.readFile('./history.json', 'UTF-8',  function read(err, data) {
+        if (err) {
+            console.log(err);
+            //throw err;
+        }
+        content = data;
+        // Invoke the next step 
+        processFile(content);         
+    });
+    
+    function processFile(fileContent) {
+        
+        res.send(fileContent);
+    }
+}
+
+function updateRecentHistory(searched) {
+    console.log('upd hist: ' + searched);
+    var content  = JSON.parse(fs.readFileSync('./history.json', 'utf8'));
+    // First I want to read the file
+    // fs.readFileSync('./history.json', 'utf8',  function read(err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         //throw err;
+    //     }
+    //     content = JSON.parse(data);
+    //     // Invoke the next step 
+    //     console.log(content);
+        
+    // });
+    
+    var updatedHistory = [];
+   // updatedHistory.push(content);
+    var nowDate = new Date();
+    var obj = { 'term': searched, 'when': moment.utc(Date.now()).local()};
+    updatedHistory = (content);
+    updatedHistory.splice(0, 0, obj);
+    updatedHistory.pop();
+    
+    fs.writeFile("history.json", JSON.stringify(updatedHistory), function(err) {
+        if (err) {
+                console.log(err);
+                //throw err;
+            }
+    	console.log("File Created @ " + Date.now());
+    
+    });
+}
+
+
+// Read local history file
+router.get('/recent', function(req, res, next) {
+    // TODO Catch error
+    console.log('reading');
+    var content;
+    // First I want to read the file
+    fs.readFile('./history.json', 'UTF-8',  function read(err, data) {
+        if (err) {
+            console.log(err);
+            //throw err;
+        }
+        content = data;
+        // Invoke the next step 
+        processFile(content);         
+    });
+    
+    function processFile(fileContent) {
+        
+        res.send(fileContent);
+    }
+
+})
+
+
+/* GET users listing. */
+router.get('/:term', function(req, res, next) {
+    
+    var searchTerm = req.params.term;
+    
+    var apikey = process.env.FLICKR_KEY;
+    var pagelimit = 10;
+    var query = req.query;
+    var page = 1;
+    if (req.query && req.query.page !== undefined) {
+        page = req.query.page;
+    }
+
+    if (req.query && req.query.pagelimit !== undefined) {
+        pagelimit = req.query.pagelimit;
+    }
+    
+    var url= 'https://api.flickr.com/services/rest/?method=flickr.photos.search&page='+page+'&per_page=' + pagelimit + '&api_key=' + apikey + '&accuracy=1&tags=' +searchTerm+ '&sort=relevance&extras=url_l,url_sq&format=json';
+    
+    var obj = {
+        snippet: "",
+        url: "",
+        context: "",
+        thumbnail: ""
+    };
+    var resultArray = [];
+    
+    callAjax(url, function(data){
+        
+        
+        var json = eval( "(" + data.substr(13) + ")" );
+        console.log(JSON.stringify(json).substr(0, 400)); 
+        if (!json.photos.photo.length > 0) {
+            console.log("No data");
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Sorry, no photos found.');
+            return;
+        } else if (json.photos.photo.length < pagelimit) 
+            pagelimit = json.photos.photo.length;
+            
+        for (var i = 0; i < pagelimit; i++) {
+            var jObj = json.photos.photo[i];
+            
+            obj.url = jObj['url_l'];
+            obj.snippet = jObj['title'];
+            obj.thumbnail = jObj['url_sq'];
+            obj.context = 'http://www.flickr.com/photos/' + jObj['owner']+ '/'+jObj['id'];
+            resultArray.push(Object.assign({}, obj));
+        }
+        updateRecentHistory(searchTerm);
+        res.send(resultArray);
+        // res.send(json.photos.photo[0]['title']);
+        
+    })
+    
+});
+
+module.exports = router;
